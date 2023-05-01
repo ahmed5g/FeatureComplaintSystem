f8b2fe065ba3248b98569991deb7605751fcceb3
Ahmed Ghorbel <ahmed.ghorbel1@esprit.tn>
2023-05-01 20:42:47 +0100

    Porject Starter

---
+++ BackEnd/FeatureComplaintSystem/FeatureComplaintSystem/src/main/java/tn/pidev/FeatureComplaintSystem/FeatureComplaintSystemApplication.java
@@ -0,0 +1,13 @@
+package tn.pidev.FeatureComplaintSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureComplaintSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureComplaintSystemApplication.class, args);
+	}
+
+}
---
+++ BackEnd/FeatureComplaintSystem/FeatureComplaintSystem/src/test/java/tn/pidev/FeatureComplaintSystem/FeatureComplaintSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.pidev.FeatureComplaintSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureComplaintSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
